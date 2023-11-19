7a07ffd1d1702cb99dba1dd6efd47e43b0e754d5
우경서 (Gyeongseo Woo) <ugs1575@gmail.com>
2023-11-19 17:18:52 +0900

    초기 세팅

---
+++ src/main/java/com/coffeekiosk/coffeekiosk/CoffeekioskApplication.java
@@ -0,0 +1,13 @@
+package com.coffeekiosk.coffeekiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeekioskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeekioskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coffeekiosk/coffeekiosk/CoffeekioskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeekiosk.coffeekiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeekioskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
