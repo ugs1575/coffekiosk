582bea8e88a24567a57481a6889f33acf7a2ae2f
우경서 (Gyeongseo Woo) <ugs1575@gmail.com>
2023-11-26 12:05:50 +0900

    test: 상품 등록 테스트 코드 작성

---
+++ src/test/java/com/coffeekiosk/coffeekiosk/ControllerTestSupport.java
@@ -0,0 +1,25 @@
+package com.coffeekiosk.coffeekiosk;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.coffeekiosk.coffeekiosk.controller.ItemApiController;
+import com.coffeekiosk.coffeekiosk.service.ItemService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebMvcTest(controllers = {
+	ItemApiController.class,
+})
+public abstract class ControllerTestSupport {
+
+	@Autowired
+	protected MockMvc mockMvc;
+
+	@Autowired
+	protected ObjectMapper objectMapper;
+
+	@MockBean
+	protected ItemService itemService;
+}
---
+++ src/test/java/com/coffeekiosk/coffeekiosk/IntegrationTestSupport.java
@@ -0,0 +1,9 @@
+package com.coffeekiosk.coffeekiosk;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+public abstract class IntegrationTestSupport {
+}
