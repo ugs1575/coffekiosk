5783b743d24a6a20c73e86e8658cee0852c5bf54
우경서 (Gyeongseo Woo) <ugs1575@gmail.com>
2024-01-06 16:27:45 +0900

    초기 사용자 data.sql 생성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into users(deleted, name, point, created_date, last_modified_date)
+values (false, '우경서', 10000, now(), now()),
+       (false, '김경서', 10000, now(), now()),
+       (false, '서경서', 10000, now(), now());
