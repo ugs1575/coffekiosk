e6e5b69a57d2f8eb091c4301e1196082868e4a19
우경서 (Gyeongseo Woo) <ugs1575@gmail.com>
2024-01-13 17:21:37 +0900

    feat: 주문조회 controller 완성

--- src/main/java/com/coffeekiosk/coffeekiosk/controller/order/OrderApiController.java
+++ src/main/java/com/coffeekiosk/coffeekiosk/controller/order/OrderApiController.java
@@ -1,18 +1,24 @@
 package com.coffeekiosk.coffeekiosk.controller.order;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.coffeekiosk.coffeekiosk.common.dto.response.ApiResponse;
 import com.coffeekiosk.coffeekiosk.common.dto.response.CreatedResponse;
 import com.coffeekiosk.coffeekiosk.controller.order.dto.request.OrderSaveRequest;
 import com.coffeekiosk.coffeekiosk.service.order.OrderService;
+import com.coffeekiosk.coffeekiosk.service.order.dto.request.OrderSearchServiceRequest;
 import com.coffeekiosk.coffeekiosk.service.order.dto.response.OrderResponse;
 
 import jakarta.validation.Valid;
@@ -34,6 +40,22 @@ public class OrderApiController {
 	@GetMapping("/{orderId}")
 	public ApiResponse<OrderResponse> findOrder(@PathVariable Long userId, @PathVariable Long orderId) {
 		OrderResponse response = orderService.findOrder(orderId, userId);
+		return ApiResponse.ok(response);
+	}
+
+	@GetMapping
+	public ApiResponse<List<OrderResponse>> findOrders(
+		@PathVariable Long userId,
+		@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") @RequestParam("startDate") LocalDateTime startDate,
+		@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") @RequestParam("endDate") LocalDateTime endDate,
+		@PageableDefault(value = 10) Pageable pageable
+	) {
+		OrderSearchServiceRequest request = OrderSearchServiceRequest.builder()
+			.startDate(startDate)
+			.endDate(endDate)
+			.build();
+
+		List<OrderResponse> response = orderService.findOrders(userId, request, pageable);
 		return ApiResponse.ok(response);
 	}
 }
