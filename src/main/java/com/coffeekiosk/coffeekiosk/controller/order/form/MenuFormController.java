4af892d0530d1be6085b54d15e275997a80f7fce
우경서 (Gyeongseo Woo) <ugs1575@gmail.com>
2024-02-11 14:46:14 +0900

    refactor : OrderFormController -> MenuFormContorller로 변경

---
+++ src/main/java/com/coffeekiosk/coffeekiosk/controller/order/form/MenuFormController.java
@@ -0,0 +1,80 @@
+package com.coffeekiosk.coffeekiosk.controller.order.form;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.coffeekiosk.coffeekiosk.common.exception.BusinessException;
+import com.coffeekiosk.coffeekiosk.controller.item.api.dto.request.ItemSearchRequest;
+import com.coffeekiosk.coffeekiosk.service.item.ItemService;
+import com.coffeekiosk.coffeekiosk.service.item.dto.response.ItemResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+public class MenuFormController {
+
+	private final ItemService itemService;
+
+	@GetMapping("/menu/list")
+	public String list(
+		Model model,
+		ItemSearchRequest itemSearchRequest,
+		@RequestParam(value = "pageSize", defaultValue = "10") int pageSize
+	) {
+		List<ItemResponse> items = itemService.findItems(null, itemSearchRequest.toServiceRequest(), pageSize + 1);
+		boolean lastPage = isLastPage(items.size(), pageSize);
+
+		if (!lastPage) {
+			items.remove(items.size() - 1);
+		}
+
+		model.addAttribute("items", items);
+		model.addAttribute("lastPage", lastPage);
+		model.addAttribute("itemSearchRequest", itemSearchRequest);
+		return "menu/listForm";
+	}
+
+	@GetMapping("/menu/nextPage")
+	public String nextPage(
+		Model model,
+		Long itemId,
+		ItemSearchRequest itemSearchRequest,
+		@RequestParam(value = "pageSize", defaultValue = "10") int pageSize
+	) {
+		List<ItemResponse> items = itemService.findItems(itemId, itemSearchRequest.toServiceRequest(), pageSize + 1);
+		boolean lastPage = isLastPage(items.size(), pageSize);
+
+		if (!lastPage) {
+			items.remove(items.size() - 1);
+		}
+
+		model.addAttribute("items", items);
+		model.addAttribute("lastPage", lastPage);
+		return "menu/listForm :: #itemList";
+	}
+
+	@GetMapping("/menu/{itemId}")
+	public String findItem(Model model, @PathVariable Long itemId) {
+		try {
+			ItemResponse item = itemService.findItem(itemId);
+			model.addAttribute("item", item);
+			return "menu/detailForm";
+		} catch (BusinessException e) {
+			return "menu/error";
+		}
+	}
+
+	private boolean isLastPage(int responseSize, int pageSize) {
+		if (responseSize == pageSize + 1) {
+			return false;
+		}
+
+		return true;
+	}
+}
--- src/main/java/com/coffeekiosk/coffeekiosk/controller/order/form/OrderFormController.java
+++ src/main/java/com/coffeekiosk/coffeekiosk/controller/order/form/OrderFormController.java
@@ -1,80 +1,36 @@
 package com.coffeekiosk.coffeekiosk.controller.order.form;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.coffeekiosk.coffeekiosk.common.exception.BusinessException;
-import com.coffeekiosk.coffeekiosk.controller.item.api.dto.request.ItemSearchRequest;
-import com.coffeekiosk.coffeekiosk.service.item.ItemService;
-import com.coffeekiosk.coffeekiosk.service.item.dto.response.ItemResponse;
+import com.coffeekiosk.coffeekiosk.controller.order.form.dto.request.OrderSaveForm;
+import com.coffeekiosk.coffeekiosk.facade.RedissonLockOrderFacade;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Controller
 public class OrderFormController {
 
-	private final ItemService itemService;
+	private final RedissonLockOrderFacade orderFacade;
 
-	@GetMapping("/menu/list")
-	public String list(
-		Model model,
-		ItemSearchRequest itemSearchRequest,
-		@RequestParam(value = "pageSize", defaultValue = "10") int pageSize
-	) {
-		List<ItemResponse> items = itemService.findItems(null, itemSearchRequest.toServiceRequest(), pageSize + 1);
-		boolean lastPage = isLastPage(items.size(), pageSize);
+	@PostMapping("/order")
+	public String create(@Valid OrderSaveForm orderSaveForm, BindingResult result, RedirectAttributes redirectAttributes) {
+		Long userId = 1L;
 
-		if (!lastPage) {
-			items.remove(items.size() - 1);
-		}
+		orderSaveForm.filterNull();
 
-		model.addAttribute("items", items);
-		model.addAttribute("lastPage", lastPage);
-		model.addAttribute("itemSearchRequest", itemSearchRequest);
-		return "menu/listForm";
-	}
+		// if (result.hasErrors()) {
+		// 	return "order/error";
+		// }
 
-	@GetMapping("/menu/nextPage")
-	public String nextPage(
-		Model model,
-		Long itemId,
-		ItemSearchRequest itemSearchRequest,
-		@RequestParam(value = "pageSize", defaultValue = "10") int pageSize
-	) {
-		List<ItemResponse> items = itemService.findItems(itemId, itemSearchRequest.toServiceRequest(), pageSize + 1);
-		boolean lastPage = isLastPage(items.size(), pageSize);
-
-		if (!lastPage) {
-			items.remove(items.size() - 1);
-		}
-
-		model.addAttribute("items", items);
-		model.addAttribute("lastPage", lastPage);
-		return "menu/listForm :: #itemList";
-	}
-
-	@GetMapping("/menu/{itemId}")
-	public String findItem(Model model, @PathVariable Long itemId) {
-		try {
-			ItemResponse item = itemService.findItem(itemId);
-			model.addAttribute("item", item);
-			return "menu/detailForm";
-		} catch (BusinessException e) {
-			return "menu/error";
-		}
-	}
-
-	private boolean isLastPage(int responseSize, int pageSize) {
-		if (responseSize == pageSize + 1) {
-			return false;
-		}
-
-		return true;
+		Long orderId = orderFacade.order(userId, orderSaveForm.toServiceRequest(), LocalDateTime.now());
+		redirectAttributes.addAttribute("orderId", orderId);
+		return "redirect:/order/{orderId}";
 	}
 }
