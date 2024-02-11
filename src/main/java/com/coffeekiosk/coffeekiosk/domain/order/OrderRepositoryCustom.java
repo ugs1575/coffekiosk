5f9135030e5594ee59efb6e76f245096b7d1fa8b
우경서 (Gyeongseo Woo) <ugs1575@gmail.com>
2024-02-11 21:20:33 +0900

    feature: 주문내역 form api

--- src/main/java/com/coffeekiosk/coffeekiosk/domain/order/OrderRepositoryCustom.java
+++ src/main/java/com/coffeekiosk/coffeekiosk/domain/order/OrderRepositoryCustom.java
@@ -2,6 +2,7 @@ package com.coffeekiosk.coffeekiosk.domain.order;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.coffeekiosk.coffeekiosk.service.order.dto.request.OrderSearchServiceRequest;
@@ -9,4 +10,5 @@ import com.coffeekiosk.coffeekiosk.service.order.dto.request.OrderSearchServiceR
 public interface OrderRepositoryCustom {
 
 	List<Order> findOrders(Long userId, OrderSearchServiceRequest request, Pageable pageable);
+	Page<Order> findPageOrders(Long userId, OrderSearchServiceRequest request, Pageable pageable);
 }
