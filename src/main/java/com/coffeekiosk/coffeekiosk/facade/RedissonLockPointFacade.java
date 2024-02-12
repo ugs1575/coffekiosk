c1d08fade95058b932435373475dd89aeed19641
우경서 (Gyeongseo Woo) <ugs1575@gmail.com>
2024-02-12 16:20:20 +0900

    fix : 포인트 충전 동시성 테스트

---
+++ src/main/java/com/coffeekiosk/coffeekiosk/facade/RedissonLockPointFacade.java
@@ -0,0 +1,51 @@
+package com.coffeekiosk.coffeekiosk.facade;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+import com.coffeekiosk.coffeekiosk.common.exception.BusinessException;
+import com.coffeekiosk.coffeekiosk.exception.ErrorCode;
+import com.coffeekiosk.coffeekiosk.service.order.OrderService;
+import com.coffeekiosk.coffeekiosk.service.order.dto.request.OrderSaveServiceRequest;
+import com.coffeekiosk.coffeekiosk.service.user.PointService;
+import com.coffeekiosk.coffeekiosk.service.user.dto.request.PointSaveServiceRequest;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class RedissonLockPointFacade {
+
+	private RedissonClient redissonClient;
+	private PointService pointService;
+
+	public RedissonLockPointFacade(RedissonClient redissonClient, PointService pointService) {
+		this.redissonClient = redissonClient;
+		this.pointService = pointService;
+	}
+
+	public void savePoint(Long userId, PointSaveServiceRequest request) {
+		RLock lock = redissonClient.getLock(userId.toString());
+
+		try {
+			boolean available = lock.tryLock(10, 1, TimeUnit.SECONDS);
+
+			if (!available) {
+				log.error("lock 획득 실패");
+				throw new BusinessException(ErrorCode.INTERNAL_SERVER_ERROR);
+			}
+
+			pointService.savePoint(userId, request);
+		} catch (BusinessException e) {
+			throw e;
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		} finally {
+			lock.unlock();
+		}
+	}
+}
