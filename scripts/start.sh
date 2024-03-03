732580b120b01f9c613e858b7d95b71bad782482
우경서 (Gyeongseo Woo) <ugs1575@gmail.com>
2024-03-03 17:41:59 +0900

    fix : 네이버 인증 로직 삭제 fix : 실서버에서 init 데이터 안하도록 수정 refactor : 타이틀 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,7 +10,7 @@ echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | head -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | grep -Ev 'plain' | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
