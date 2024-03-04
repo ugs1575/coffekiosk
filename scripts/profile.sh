e8ec3236d07bed9661c85fc1d1ee01980b19ea53
우경서 (Gyeongseo Woo) <ugs1575@gmail.com>
2024-03-04 22:23:34 +0900

    ci : 연결안된 profile 찾는 script 수정

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,12 +1,12 @@
 function find_idle_profile()
 {
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" https://coffeekiosk.kro.kr/profile)
 
     if [ ${RESPONSE_CODE} -ge 400 ]
     then
         CURRENT_PROFILE=real2
     else
-        CURRENT_PROFILE=$(curl -s http://localhost/profile)
+        CURRENT_PROFILE=$(curl -s https://coffeekiosk.kro.kr/profile)
     fi
 
     if [ ${CURRENT_PROFILE} == real1 ]
